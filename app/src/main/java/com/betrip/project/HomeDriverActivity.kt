51ec5ddc52894858bc4908b0c96b60f8211086fc
Miner <54141767+Apolosmllb@users.noreply.github.com>
2022-09-30 21:45:06 -0500

    Update HomeDriverActivity.kt

--- app/src/main/java/com/betrip/project/HomeDriverActivity.kt
+++ app/src/main/java/com/betrip/project/HomeDriverActivity.kt
@@ -1,1 +1,32 @@
+package com.betrip.project
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import com.betrip.project.drivers.CreateMenuDriverActivity
+import com.betrip.project.drivers.CreateTripActivity
+import com.betrip.project.drivers.Driver_profile
+
+class HomeDriverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_driver)
+
+        loadProfile()
+    }
+
+    private fun loadProfile() {
+        val driverProfile = findViewById<Button>(R.id.btGoToProfile)
+        val createMenu= findViewById<Button>(R.id.btGoCreate)
+
+        driverProfile.setOnClickListener {
+            startActivity(Intent(this, CreateMenuDriverActivity::class.java))
+        }
+
+        createMenu.setOnClickListener {
+            startActivity(Intent(this, CreateTripActivity::class.java))
+        }
+    }
+}
